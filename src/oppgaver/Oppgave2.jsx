95a882c8a96be4b738fa0daa2c3fa3f9c1a79731
Anna Li <halloannali@gmail.com>
2022-04-25 20:45:33 +0200

    endret oppgavetekst i oppg 1 og 2

--- src/oppgaver/Oppgave2.jsx
+++ src/oppgaver/Oppgave2.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-// 1. Opprett en context. Kall den ThemeContext og sett default-verdi til null.
-
 export function Oppgave2() {
+  // I denne oppgaven skal du lage en tema-bytter som bytter mellom svart og hvitt tema ved å trykke på en button.
+  // 1. Opprett en context. Kall den ThemeContext og sett default-verdi til null.
   // 2. Opprett en "theme" state og sett default til "light"
   // 3. Lag en metode toggleTheme som setter theme til "dark" hvis theme er "light" og "light" ellers.
 
   return null;
   // 4. Sett opp en context provider som tar imot "theme" som value.
   // Inni provideren skal det være en div: <div className={`full-size theme-bg--${theme}`}></div>
-  // Inni denne div'en skal du lage en button som kjører toggleTheme metoden ved onClick og <Text/>-komponenten
+  // Inni denne div'en skal ha to ting:
+  // - 1. lage en button som kjører toggleTheme-metoden ved onClick
+  // - 2. <Text/>-komponenten
 }
 
 function Text() {
